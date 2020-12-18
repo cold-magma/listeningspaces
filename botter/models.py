f87738aed1f80db11e8f2c06318d3ecfb49aa8ac
Amal Nair <amalrocks29@gmail.com>
2020-12-18 21:32:35 +0530

    Update models.py

--- botter/models.py
+++ botter/models.py
@@ -4,8 +4,8 @@ from flask_login import UserMixin
 class User(db.Model,UserMixin):
     email_id = db.Column(db.String(100), primary_key=True, unique=True)
     password = db.Column(db.String(100))
-    access_token = db.Column(db.String(100))
-    refresh_token = db.Column(db.String(100))
+    access_token = db.Column(db.String(500))
+    refresh_token = db.Column(db.String(500))
     token_time = db.Column(db.Integer)
     name = db.Column(db.String(100))
     room_key = db.Column(db.String(100))
