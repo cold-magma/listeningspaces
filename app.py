85c5fe3ac078f9e79fe51b2f291dffa7fc2ab9d7
Amal Nair <amalrocks29@gmail.com>
2020-12-18 21:13:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from botter import create_app,socketio
+from botter import create_app,socketio,db
 
 app = create_app()
+
+db.create_all(app=app)
 
 if __name__ == "__main__":
     socketio.run(app)
