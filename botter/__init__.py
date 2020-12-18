c7be8bbf6f86652a574b8b719b36776c2194166e
Amal Nair <amalrocks29@gmail.com>
2020-12-18 21:52:41 +0530

    Update __init__.py

--- botter/__init__.py
+++ botter/__init__.py
@@ -35,6 +35,6 @@ def create_app():
     from .main import main
     app.register_blueprint(main)
 
-    socketio.init_app(app)
+    socketio.init_app(app, cors_allowed_origins="*")
     
     return app
